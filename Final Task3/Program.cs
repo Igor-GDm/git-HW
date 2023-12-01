e3c96e796b2ea0ac8418fe8ebefac6fd84127cfc
Igor-GDm <147552168+Igor-GDm@users.noreply.github.com>
2023-12-02 02:40:42 +0500

    Add files via upload

---
+++ Final Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете
+//его элементы, начиная с конца. Использовать
+//рекурсию, не использовать циклы.
+
+int[] myArray = {1,2,5,3,10,34};
+PrinArray(myArray,5);
+static void PrinArray(int[] myArray, int i = 0)
+{
+    if (i < myArray.Length)
+    {
+        Console.WriteLine(myArray[i]);
+        PrinArray(myArray, i - 1);
+    }
+}
